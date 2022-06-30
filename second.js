79befa64cf81bda4bfef605b45d3c4f3512d7352
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2022-06-30 20:08:13 +0530

    solved second question

--- second.js
+++ second.js
@@ -1,20 +1,18 @@
 // To check if given number is prime or not
 
+
 function isPrime(num) {
-  if (num === 2 || num === 1) {
-    return "Prime Number";
+  if (num <= 1) {
+    return " Neither Prime nor Composite Number";
   }
-  if (num > 2) {
-    for (var i = 2; i < num; i++) {
-      if (num % i !== 0) {
-        return "Prime Number";
-      } else if (num === i * i) {
-        return "Not a Prime Number";
-      } else {
-        return "Not a Prime Number";
-      }
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return "Not a Prime Number";
     }
   }
+  return "Prime Number";
 }
 
 console.log(isPrime(4));
+
+
