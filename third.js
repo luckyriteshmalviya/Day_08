0713af7af85fb84202e43655219441b1b8ce16e4
Ritesh Malviya <luckyriteshmalviya@gmail.com>
2022-06-30 20:13:23 +0530

    solved third question

--- third.js
+++ third.js
@@ -1,1 +1,22 @@
 // To print all the prime numbers from 2 to 100
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log(isPrime(100));
+
+function listPrimeNumber(start, end) {
+  for (let i = start; i <= end; i++) {
+    isPrime(i) ? console.log(i) : null;
+  }
+}
+console.log(listPrimeNumber(0, 100));
